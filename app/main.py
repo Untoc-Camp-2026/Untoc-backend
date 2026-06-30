4232a26cf37be5cf63d4d480c60770b2dd17922c
minecoby <127065808+minecoby@users.noreply.github.com>
2026-06-30 15:21:03 +0900

    데이터베이스 구성

--- app/main.py
+++ app/main.py
@@ -1,6 +1,10 @@
 from fastapi import FastAPI
+from core.base import Base
+from core.database import engine
 app = FastAPI()
 
-@app.get("/")
-def read_root():
-    return {"message": "Hello World"}
+
+@app.on_event("startup")
+async def startup():
+    async with engine.begin() as conn:
+        await conn.run_sync(Base.metadata.create_all)
