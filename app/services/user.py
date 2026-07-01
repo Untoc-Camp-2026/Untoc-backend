6df52c9f6de2d900264fdfdfb9071762b1e05e52
parkfrog1718 <parkfrog1718@naver.com>
2026-07-01 17:45:40 +0900

    회원가입구현

--- app/services/user.py
+++ app/services/user.py
@@ -2,6 +2,7 @@ from sqlalchemy.orm import Session
 from sqlalchemy.future  import select   
 from models.user import User
 from core.security import verify_password
+from core.security import get_password_hash
 #사용자 인증 로직 (해시)
 async def authenticate_user(db, user_id: str, password: str):
     stmt = select(User).where(User.user_id == user_id)
@@ -15,3 +16,27 @@ async def authenticate_user(db, user_id: str, password: str):
         return False
         
     return user
+#회원가입 로직
+async def create_user(db, user_data):
+    stmt = select(User).where(User.user_id == user_data.user_id)
+    result = await db.execute(stmt)
+    if result.scalars().first():
+        return None  
+
+    hashed_password = get_password_hash(user_data.password)
+
+
+    new_user = User(
+        user_id=user_data.user_id,
+        password=hashed_password,
+        name=user_data.name,
+        role=user_data.role,
+        generation=user_data.generation,
+        activity_status=True,
+        admin_status=True
+    )
+
+    db.add(new_user)
+    await db.commit()
+    await db.refresh(new_user)
+    return new_user
