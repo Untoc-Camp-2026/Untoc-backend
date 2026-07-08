26d1f7c39b83334188259a84775889c7937765d3
김준우 <150007870+Applefai1221@users.noreply.github.com>
2026-07-08 20:32:33 +0900

    수정

--- app/main.py
+++ app/main.py
@@ -1,27 +1,27 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi.staticfiles import StaticFiles
+
 from core.base import Base
 from core.database import engine
-from models import user,attendance
-from fastapi.middleware.cors import CORSMiddleware
-from api import user, attendance, board, calendar, gallery, camp
 
 from models.user import User
 from models.calendar import EventCategory, CalendarEvent
 from models.camp import CampProject
-from api import user, attendance, board, calendar, file
+from models import user, attendance
 
-from models.user import User
-from models.calendar import EventCategory, CalendarEvent
-from fastapi.staticfiles import StaticFiles
+from api import user, attendance, board, calendar, gallery, camp, file
 
 app = FastAPI()
 
+# ⭕ 프론트엔드 주소(포트 없음)를 정확하게 허용하도록 수정했습니다.
 origins = [
     "http://127.0.0.1",
     "http://localhost",
     "http://localhost:3000",
-    "http://161.33.19.121:8000"
+    "http://161.33.19.121"  
 ]
+
 app.mount("/uploads", StaticFiles(directory="uploads"), name="uploads")
 
 app.add_middleware(
